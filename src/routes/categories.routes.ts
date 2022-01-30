7209390e5bb40d974eb7007367f86fb80b8d9ae1
Jean Martins <jeanmrtns4@gmail.com>
2022-01-30 03:09:04 -0300

    Upload categories csv

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
+import { importCategoryController } from '../modules/cars/useCases/importCategory';
 import { listCategoriesController } from '../modules/cars/useCases/listCategories';
 
 const categoriesRoutes = Router();
+const upload = multer({ dest: './tmp' });
 
 categoriesRoutes.get('/', (req, res) => {
   return listCategoriesController.handle(req, res);
@@ -11,6 +14,10 @@ categoriesRoutes.get('/', (req, res) => {
 
 categoriesRoutes.post('/', (req, res) => {
   return createCategoryController.handle(req, res);
+});
+
+categoriesRoutes.post('/import', upload.single('file'), (req, res) => {
+  return importCategoryController.handle(req, res);
 });
 
 export { categoriesRoutes };
